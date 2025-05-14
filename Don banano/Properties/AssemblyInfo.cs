e9a5b2ea7e34d26a9fd4ca856263ea9f1dbc0bfa
Mateo Correa Restrepo <mateo.correa926@pascualbravo.edu.co>
2025-05-13 20:28:59 -0500

    ignore

---
+++ Don banano/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Don banano")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Don banano")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("ed36d6f1-9b77-49c1-96d5-cd12f1ff62e0")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Don banano/clsProducto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Don_banano
+{
+    internal class clsProducto
+    {
+            public string Id { get; set; }
+            public string Nombre { get; set; }
+            public int Cantidad { get; set; }
+            public decimal Precio { get; set; }
+
+            public Producto(string id, string nombre, int cantidad, decimal precio)
+            {
+                this.Id = id;
+                this.Nombre = nombre;
+                this.Cantidad = cantidad;
+                this.Precio = precio;
+            }
+        
+
+    }
+}
