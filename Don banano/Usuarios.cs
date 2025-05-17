0a31a5f618c4b6d4bbc4357f170e300154b70e98
Valen-17 <valeperezcar2015@gmail.com>
2025-05-17 09:51:05 -0500

    Vendedores como usuarios

---
+++ Don banano/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿namespace Don_banano
+{
+    public class Usuarios
+    {
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Rol { get; set; }
+
+        public Usuarios(string usuario, string contraseña, string rol)
+        {
+            Usuario = usuario;
+            Contraseña = contraseña;
+            Rol = rol;
+        }
+    }
+}
---
+++ Don banano/UsuariosDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Don_banano
+{
+    internal class UsuariosDB
+    {
+        public static List<Usuarios> ListaUsuarios { get; } = new List<Usuarios>();
+    }
+}
