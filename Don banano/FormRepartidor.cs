2878ca01f7a0a6edc5bb78d7c41acb6284a9d429
Daniela <danielamq2014@hotmail.com>
2025-05-17 10:07:38 -0500

    Actualización código repartidor

--- Don banano/FormRepartidor.cs
+++ Don banano/FormRepartidor.cs
@@ -16,5 +16,57 @@ namespace Don_banano
         {
             InitializeComponent();
         }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            int x = (this.ClientSize.Width - panel1_repartidor.Width) / 2;
+            int y = (this.ClientSize.Height - panel1_repartidor.Height) / 2;
+            panel1_repartidor.Location = new Point(x, y);
+            panel1_repartidor.Visible = true;
+            panel1_repartidor.BringToFront();
+        }
+
+        private void guna2Button11_Click(object sender, EventArgs e)
+        {
+            panel1_repartidor.Visible = false;
+            panel1_repartidor.SendToBack();
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            int x = (this.ClientSize.Width - panel2_completadas.Width) / 2;
+            int y = (this.ClientSize.Height - panel2_completadas.Height) / 2;
+            panel2_completadas.Location = new Point(x, y);
+            panel2_completadas.Visible = true;
+            panel2_completadas.BringToFront();
+        }
+
+        private void guna2Button10_Click(object sender, EventArgs e)
+        {
+            panel2_completadas.Visible = false;
+            panel2_completadas.SendToBack();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            int x = (this.ClientSize.Width - panel3_actual.Width) / 2;
+            int y = (this.ClientSize.Height - panel3_actual.Height) / 2;
+            panel3_actual.Location = new Point(x, y);
+            panel3_actual.Visible = true;
+            panel3_actual.BringToFront();
+        }
+
+        private void btn_cerrarR_Click(object sender, EventArgs e)
+        {
+            panel3_actual.Visible = false;
+            panel3_actual.SendToBack();
+        }
+
+        private void guna2Button7_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();   
+            this.Hide();
+        }
     }
 }
